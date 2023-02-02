38696ae6bd02f341ceed49b3a4f25aa7994e4b05
Ali <ali-a-ahmed1097@hotmail.com>
2023-02-01 21:06:19 -0500

    Created the header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
 
 export default function App() {
-    return (<div>Hello!</div>);
+    const [data, setData] = React.useState([]);
+    const [score, setScore] = React.useState(0);
+
+    React.useEffect(() => {
+        fetch('https://botw-compendium.herokuapp.com/api/v2/category/monsters')
+        .then(response => response.json())
+        .then(response => setData(response.data));
+    }, []);
+
+    return (
+        <div>
+            <Header score={score} />
+        </div>
+    );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <h1>Hyrule Monster Memory Game</h1>
+            <div className="score">Score: {props.score} | Best: 0</div>
+        </div>
+    );
+}
