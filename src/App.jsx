573e47ab5b01619578a7b8706e06d8bb4073b34c
Ali <ali-a-ahmed1097@hotmail.com>
2023-02-02 00:41:25 -0500

    Finished the game. But my code was so buggy.
    I have no idea why that is. Could've completed it sooner if React was
    behaving properly.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import shuffle from './knuth-fisher-yates-shuffle';
 export default function App() {
     const [data, setData] = React.useState([]);
     const [score, setScore] = React.useState(0);
+    const [best, setBest] = React.useState(0);
     const [level, setLevel] = React.useState(1);
-    const [cards, setCards] = React.useState([]);
     const [moves, setMoves] = React.useState([]);
 
     React.useEffect(() => {
@@ -22,37 +22,34 @@ export default function App() {
     }, [level]);
 
     function play(id) {
-        console.log(moves);
         if (moves.includes(id)) { // Player failed, reset game
             setLevel(1);
             setScore(0);
+            setMoves([]);
         } else {
             setMoves(oldMoves => [...oldMoves, id]);
             setScore(oldScore => oldScore + 1);
-            shuffleCards();
+            setBest(oldBest => score > oldBest ? score : oldBest);
         }
 
-        if (moves.length === cards.length) {
+        if (moves.length === cards.length - 1) { // Moves does not get updated fast enough so we have to do this...
             setLevel(oldLevel => oldLevel + 1);
             setMoves([]);
         }
-    }
 
-    console.log(moves);
+        shuffleCards();
+    }
     
     function shuffleCards() {
         setData(shuffle(data));
-        setCards(data.map(card => <Card key={card.id} name={card.name} image={card.image} play={() => play(card.id)} />));
     }
 
-    React.useEffect(() => {
-        setCards(data.map(card => <Card key={card.id} name={card.name} image={card.image} play={() => play(card.id)} />));
-    }, [data]);
+    const cards = data.map(card => <Card key={card.id} name={card.name} image={card.image} play={() => play(card.id)} />);
 
     return (
         <div>
-            <Header score={score} />
-            <div>Choose your next Monster! Level {level}</div>
+            <Header score={score} best={best} />
+            <div className="level">Choose your next Monster! Level {level}</div>
             <div className="cards">
                 {cards}
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
     return (
         <div className="header">
             <h1>Hyrule Monster Memory Game</h1>
-            <div className="score">Score: {props.score} | Best: 0</div>
+            <div className="score">Score: {props.score} | Best: {props.best}</div>
         </div>
     );
 }
